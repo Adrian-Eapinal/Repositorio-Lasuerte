5e86b3764e2e95645f86839754283420023b8bd7
Creacion del proyecto <lacomputer07@gmail.com>
2020-10-07 22:26:53 -0400

    PROGRAMACION DEL FORMULARIO EQUIPO

---
+++ GESTION/Sistema.Entidades/NEquipo.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Sistema.Entidades
+{
+   public  class NEquipo
+    {
+        public int IdEquipo { get; set }
+        public int IdCategoria { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Serial { get; set; }
+        public string Marca { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public bool Estado { get; set; }
+    }
+}
